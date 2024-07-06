85d36f116dba80ce05175c39595d5e786c7e45d0
Sharvika Barapatre <barapatre.s@northeastern.edu>
2024-07-06 11:54:32 -0400

    experience changes added

--- MyPortFolio/src/components/Profile.js
+++ MyPortFolio/src/components/Profile.js
@@ -6,8 +6,11 @@ import SocialHandles from "./SocialHandles";
 
 const Profile = () => {
   return (
+    
     <section className="text-gray-600 body-font pt-16 lg:min-h-75vh">
+
       <div className="p-5 mx-auto gap-2 flex flex-col md:pt-12 md:px-7 lg:py-20 lg:flex-row-reverse items-center min-h-fit">
+        
         <div
           data-aos="zoom-in-up"
           data-aos-duration="1000"
@@ -15,12 +18,18 @@ const Profile = () => {
           className="w-5/6 sm:max-w-xs md:max-w-sm lg:max-w-md sm:w-2/6 lg:mr-10 xl:mr-20 lg:p-5 lg:w-1/3 xl:w-1/4 "
           style={{ marginBottom: '-10px' }}
         >
-          <img
+     
+           <img
             className="object-cover object-center bg-[#b9aeb6] border-4 border-white pointer-events-none rounded-full backdrop-contrast-200 backdrop-brightness-200"
             alt="Sharvika Barapatre"
             src={require("../assets/Images/zoomed.png")}
           />
+
+         
+
+
         </div>
+     
         <div className="lg:flex-grow lg:pr-4 lg:mr-14 flex flex-col md:mb-0 items-center text-center xl:scale-105">
           {/* <SocialHandles /> */}
           <h2
@@ -29,7 +38,7 @@ const Profile = () => {
             data-aos-once="false"
             className="title-font md:text-3xl text-2xl mb-4 text-center font-medium text-white"
           >
-            Hello, I am <span className="text-dark-orange">Sharvika Barapatre</span>
+            Hello , I am <span className="text-dark-orange">Sharvika Barapatre</span>
           </h2>
           <div
             data-aos="zoom-in-up"
@@ -39,7 +48,7 @@ const Profile = () => {
           >
             &nbsp;
             <Typewriter
-              words={["Frontend Developer", "Full Stack Engineer", "Cloud Enthusiast"]}
+              words={["Frontend Developer", "Full Stack Engineer", "Cloud Enthusiasist"]}
               loop={false}
               typeSpeed={100}
               deleteSpeed={100}
@@ -52,6 +61,7 @@ const Profile = () => {
             data-aos-once="false"
             className="mb-2 text-white text-lg md:text-xl leading-relaxed"
           >
+    
             Passionate about building applications and solving real-world problems.
           </p>
           <p
@@ -79,25 +89,26 @@ const Profile = () => {
                 Hire Me
               </Link>
             </button>
-            <button className="inline-flex font-medium text-white bg-dark-orange border-2 border-dark-orange py-3 px-7 focus:outline-none hover:bg-cornsilk hover:border-dark-orange hover:text-black rounded-full text-md xl:px-10">
-              <a
-                href="/resume.pdf"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Get Resume
-              </a>
-            </button>
+       
+              <button className="inline-flex font-medium text-white bg-dark-orange border-2 border-dark-orange py-3 px-7 focus:outline-none hover:bg-cornsilk hover:border-dark-orange hover:text-black rounded-full text-md xl:px-10">
+                <a
+                  href="/resume.pdf"
+            
+                  rel="noreferrer"
+                >
+                  Get Resume
+                </a>
+              </button>
           </div>
         </div>
       </div>
-      {/* Wave */}
+      {/* Wave  */}
       <div className="lg:mt-6">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 24 150 28"
           preserveAspectRatio="none"
-          className="w-full h-18 sm:h-20 lg:h-[15vh] border-0 border-red-500"
+          className=" w-full h-18 sm:h-20 lg:h-[15vh] border-0 border-red-500 "
         >
           <defs>
             <path
@@ -105,27 +116,27 @@ const Profile = () => {
               d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18v44h-352z"
             />
           </defs>
-          <g className="waves">
+          <g class="waves">
             <use
               xlinkHref="#gentle-wave"
               x="50"
               y="0"
               fill="#f5f5f5"
-              fillOpacity=".2"
+              fill-opacity=".2"
             />
             <use
               xlinkHref="#gentle-wave"
               x="50"
               y="3"
               fill="#f5f5f5"
-              fillOpacity=".5"
+              fill-opacity=".5"
             />
             <use
               xlinkHref="#gentle-wave"
               x="50"
               y="6"
               fill="#f5f5f5"
-              fillOpacity=".9"
+              fill-opacity=".9"
             />
           </g>
         </svg>
